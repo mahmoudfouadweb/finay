f82e786595ef317862ea5c88e66690c79cd62ff7
Mahmoud Fouad <mfapolice@gmail.com>
2023-01-06 07:45:36 +0200

    update

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const sidebar = () => {
+const Sidebar = () => {
   return (
-    <div>sidebar</div>
+    <aside className='sidebar'>Sidebar</aside>
   )
 }
 
-export default sidebar
+export default Sidebar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
-      
+      <Sidebar />
       <Footer />
     </div>
   )
